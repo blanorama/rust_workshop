ed1959c5a37f4a8bc7e18e3fc6fb7b1c35e621bb
Emanuel Vollmer <xedon.ev@googlemail.com>
2021-08-09 18:43:45 +0000

    Add flow control and functions

---
+++ examples/3_flow_control.rs
@@ -0,0 +1,93 @@
+fn main() {
+    if true {
+        println!("true strory");
+    } else {
+        println!("if u see that, rust is broken");
+    }
+
+    // there is no ternary operator in rust but u can use if/else
+    let x = if true == false { 1 } else { 2 };
+    println!("x: {}", x);
+
+    // every block in rust can return something if it is not ended by a ;
+    let x = { "block return" };
+    println!("x: {}", x);
+
+    let x = match x {
+        "block" => 1,
+        "block return" => 2,
+        _ => 3,
+    };
+    println!("x: {}", x);
+
+    let tuple = (1, 3);
+    match tuple {
+        (1, 3) => {
+            println!("first is 1 second is 3")
+        }
+        (1, _) => {
+            println!("first is 1")
+        }
+        _ => {
+            println!("none matched")
+        }
+    };
+
+    #[derive(Debug)]
+    enum RemoteValue {
+        NotLoaded(),
+        Loading(String),
+        Loaded(String),
+        Error(String),
+    }
+
+    let x = RemoteValue::Loaded("Ok".into());
+
+    match x {
+        RemoteValue::Error(x) => println!("{}", x),
+        value => println!("{:#?}", value),
+    }
+
+    let mut x = 1;
+
+    loop {
+        if x % 3 == 0 {
+            break;
+        }
+        println!("loop x was: {}", x);
+
+        x = x + 1;
+    }
+
+    let mut x = 1;
+
+    while x % 3 != 0 {
+        println!("while x was: {}", x);
+        x = x + 1;
+    }
+
+    for x in 1..3 {
+        println!("for x was: {}", x)
+    }
+
+    let vector = vec!["One", "Two", "Three"];
+
+    for elem in vector {
+        println!("for elem was: {}", elem)
+    }
+
+    let mut remote = RemoteValue::NotLoaded();
+
+    while let RemoteValue::NotLoaded() = remote {
+        match remote {
+            RemoteValue::NotLoaded() => remote = RemoteValue::Loading("google.de".into()),
+            _ => {}
+        }
+    }
+
+    println!("value: {:#?}", remote);
+
+    if let RemoteValue::Loading(x) = remote {
+        println!("{}", x);
+    }
+}
---
+++ examples/4_functions.rs
@@ -0,0 +1,32 @@
+fn add(x: i64, y: i64) -> i64 {
+    return x + y;
+}
+
+fn main() {
+    let multiply = |x: i64, y: i64| {
+        return x * y;
+    };
+
+    let x = multiply(2, 2);
+
+    println!("x was {}", x);
+
+    let x = add(1, 2);
+
+    println!("x was {}", x);
+
+    fn is_even<F>(calc_function: F, x: i64, y: i64) -> bool
+    where
+        F: Fn(i64, i64) -> i64,
+    {
+        calc_function(x, y) % 2 == 0
+    }
+
+    let x = is_even(add, 4, 4);
+
+    println!("add x was {}", x);
+
+    let x = is_even(multiply, 3, 4);
+
+    println!("add x was {}", x);
+}
